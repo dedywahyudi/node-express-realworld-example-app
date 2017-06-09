58d5c687afd20bc85413d0db46c06e82db022770
lunarkid <d3dyw4hyud1@gmail.com>
2017-06-09 23:20:18 +0700

    MONGOLAB_PURPLE_URI

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ if (!isProduction) {
 }
 
 if(isProduction){
-  mongoose.connect(process.env.MONGODB_URI);
+  mongoose.connect(process.env.MONGOLAB_PURPLE_URI);
 } else {
   mongoose.connect('mongodb://localhost/conduit');
   mongoose.set('debug', true);
